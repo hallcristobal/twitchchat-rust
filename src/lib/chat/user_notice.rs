4940af4ee5b65f4451089c2133e17f4e37aa9289
Cristobal Hall-Ramos <crishallramos@yahoo.com>
2018-07-08 19:54:45 -0400

    WIP

--- src/lib/chat/user_notice.rs
+++ src/lib/chat/user_notice.rs
@@ -1,5 +1,7 @@
 use super::{Badge, Emote, UserNoticeType, UserType};
-use std::time::Instant;
+use chrono::NaiveDateTime;
+use std::collections::HashMap;
+use super::Result;
 
 #[derive(Eq, PartialEq, Debug)]
 pub struct UserNotice {
@@ -14,8 +16,24 @@ pub struct UserNotice {
     room_id: usize,
     subscriber: bool,
     system_msg: String,
-    tmi_sent_ts: Instant,
+    tmi_sent_ts: NaiveDateTime,
     turbo: bool,
     user_id: usize,
     user_type: UserType,
+}
+
+impl UserNotice {
+    pub fn parse(tags: HashMap<&str, Option<&str>>) -> Result<UserNotice> {
+        let badges = if let Some(badges) = get_value!("badges", tags) {
+            badges.split(',').map(|v| v.parse().unwrap()).collect()
+        } else {
+            Vec::new()
+        };
+
+        Ok(UserNotice {
+            badges,
+            color: get_value_parse!("color", tags),
+            
+        })
+    }
 }
